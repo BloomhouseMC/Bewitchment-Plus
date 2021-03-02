41b0629a767d31e43130e7109528ee479075f19f
Sunconure11 <eyespot7b@aol.com>
2021-03-02 11:05:43 -0500

    Update BewitchmentPlusMod.java

--- src/main/java/net/bewitchmentplus/BewitchmentPlusMod.java
+++ src/main/java/net/bewitchmentplus/BewitchmentPlusMod.java
@@ -2,7 +2,7 @@ package net.bewitchmentplus;
 
 import net.fabricmc.api.ModInitializer;
 
-public class BewitchmentPlus implements ModInitializer {
+public class BewitchmentPlusMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
