ad7b19aecaa340eaa5f4b476683ec65c8429500c
Sunconure11 <eyespot7b@aol.com>
2021-04-01 08:28:40 -0400

    Update DrudenEntity.java

--- src/main/java/net/bewitchmentplus/common/entity/living/DrudenEntity.java
+++ src/main/java/net/bewitchmentplus/common/entity/living/DrudenEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.world.World;
@@ -18,6 +19,12 @@ public class DrudenEntity extends BWHostileEntity {
 
 	public static DefaultAttributeContainer.Builder createAttributes() {
 		return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 25.00D).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 6.0D).add(EntityAttributes.GENERIC_ARMOR, 4.0D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5D).add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 1.0D).add(EntityAttributes.GENERIC_FOLLOW_RANGE, 16.0D);
+	}
+
+	public void tick() {
+		super.tick();
+		if (this.isOnFire())
+			this.applyDamage(DamageSource.ON_FIRE, 6);
 	}
 
 	@Override
