65d31ec36701d8300dc26cec0e79465e98b065dc
Sunconure11 <eyespot7b@aol.com>
2021-04-02 06:42:52 -0400

    Update DrudenEntity.java

--- src/main/java/net/bewitchmentplus/common/entity/living/DrudenEntity.java
+++ src/main/java/net/bewitchmentplus/common/entity/living/DrudenEntity.java
@@ -2,19 +2,33 @@ package net.bewitchmentplus.common.entity.living;
 
 import moriyashiine.bewitchment.api.BewitchmentAPI;
 import moriyashiine.bewitchment.common.entity.living.util.BWHostileEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
 @SuppressWarnings("ALL")
 public class DrudenEntity extends BWHostileEntity {
+	public int attackTick = 0;
+
 	public DrudenEntity(EntityType<? extends HostileEntity> entityType, World world) {
 		super(entityType, world);
+		this.setPathfindingPenalty(PathNodeType.DANGER_OTHER, 0.0F);
+		this.setPathfindingPenalty(PathNodeType.DAMAGE_OTHER, 0.0F);
+		this.setPathfindingPenalty(PathNodeType.DAMAGE_CACTUS, 0.0F);
+		this.setPathfindingPenalty(PathNodeType.DAMAGE_CACTUS, 0.0F);
 		experiencePoints = 5;
 	}
 
@@ -29,6 +43,39 @@ public class DrudenEntity extends BWHostileEntity {
 	}
 
 	@Override
+	public boolean tryAttack(Entity target) {
+		boolean flag = super.tryAttack(target);
+		if (flag) {
+			toggleAttack(false);
+			if (target instanceof LivingEntity) {
+				((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 100));
+			}
+		}
+		return flag;
+	}
+
+	public void toggleAttack(boolean attacking) {
+		if (attacking) {
+			attackTick = 11;
+			world.sendEntityStatus(this, (byte) 4);
+		} else {
+			attackTick = 2;
+			world.sendEntityStatus(this, (byte) 5);
+		}
+	}
+
+	@Override
+	protected void initGoals() {
+		goalSelector.add(0, new SwimGoal(this));
+		goalSelector.add(1, new MeleeAttackGoal(this, 1, true));
+		goalSelector.add(2, new WanderAroundFarGoal(this, 1));
+		goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 8));
+		goalSelector.add(3, new LookAroundGoal(this));
+		targetSelector.add(0, new RevengeGoal(this));
+		targetSelector.add(1, new FollowTargetGoal<>(this, LivingEntity.class, 10, true, false, entity -> entity instanceof PlayerEntity || entity instanceof MerchantEntity || entity.getGroup() == EntityGroup.ILLAGER));
+	}
+
+	@Override
 	protected boolean hasShiny() {
 		return true;
 	}
@@ -39,6 +86,6 @@ public class DrudenEntity extends BWHostileEntity {
 
 	@Override
 	public int getVariants() {
-		return 0;
+		return 9;
 	}
 }
