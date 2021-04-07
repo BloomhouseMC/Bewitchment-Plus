90d8d680759b69b84cc2908cabc33b28fe83ab1e
Sunconure11 <eyespot7b@aol.com>
2021-04-07 10:14:00 -0400

    Silver tags

--- src/main/java/net/bewitchmentplus/mixin/SilverMixin.java
+++ src/main/java/net/bewitchmentplus/mixin/SilverMixin.java
@@ -1,19 +1,28 @@
 package net.bewitchmentplus.mixin;
 
 import moriyashiine.bewitchment.api.BewitchmentAPI;
-import net.bewitchmentplus.common.registry.BWPTags;
+import moriyashiine.bewitchment.common.entity.projectile.SilverArrowEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.EntityDamageSource;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 
+import static net.bewitchmentplus.api.BWPAPI.isHoldingSilver;
+
+
 @SuppressWarnings("ConstantConditions")
 @Mixin(BewitchmentAPI.class)
-public class SilverMixin {
-
-	@Inject(method = "isHoldingSilver", at = @At("TAIL"))
-	private static boolean isHoldingSilver(LivingEntity livingEntity, Hand hand) {
-		return BWPTags.SILVER_TOOLS.contains(livingEntity.getStackInHand(hand).getItem());
+public abstract class SilverMixin {
+	@Inject(method = "isSourceFromSilver", at = @At("TAIL"))
+	private static boolean isSourceFromSilver(DamageSource source) {
+		Entity attacker = source.getSource();
+		if (source instanceof EntityDamageSource && ((EntityDamageSource) source).isThorns()) {
+			return false;
+		}
+		return (attacker instanceof LivingEntity && isHoldingSilver((LivingEntity) attacker, Hand.MAIN_HAND)) || attacker instanceof SilverArrowEntity;
 	}
 }
