9b307efe4f5dfa45348d2a22a8ce3009ef1c8353
Sunconure11 <eyespot7b@aol.com>
2021-04-07 14:13:34 -0400

    Update SilverArmorMixin.java

--- src/main/java/net/bewitchmentplus/mixin/SilverArmorMixin.java
+++ src/main/java/net/bewitchmentplus/mixin/SilverArmorMixin.java
@@ -8,11 +8,14 @@ import net.bewitchmentplus.common.registry.BWPTags;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.EntityDamageSource;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
@@ -20,6 +23,25 @@ public abstract class SilverArmorMixin extends Entity {
 
 	public SilverArmorMixin(EntityType<?> type, World world) {
 		super(type, world);
+	}
+
+	@ModifyVariable(method = "applyArmorToDamage", at = @At("HEAD"))
+	private float modifyDamage1(float amount, DamageSource source) {
+		if (!world.isClient) {
+			Entity trueSource = source.getAttacker();
+			Entity directSource = source.getSource();
+			if (!(source instanceof EntityDamageSource && ((EntityDamageSource) source).isThorns()) && directSource instanceof LivingEntity) {
+				LivingEntity livingAttacker = (LivingEntity) directSource;
+				if (BewitchmentAPI.isWeakToSilver(livingAttacker)) {
+					int armorPieces = BWUtil.getArmorPieces(livingAttacker, stack -> BWPTags.SILVER_ARMOR.contains(stack.getItem()));
+					if (armorPieces > 0) {
+						directSource.damage(DamageSource.thorns(this), armorPieces);
+					}
+					amount *= (1 - (0.125f * armorPieces));
+				}
+			}
+		}
+		return amount;
 	}
 
 	@Inject(method = "tick", at = @At("TAIL"))
