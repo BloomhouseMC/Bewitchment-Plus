3cd56d107b39abee5341df1334d100167304a955
Sunconure11 <eyespot7b@aol.com>
2021-04-08 11:24:08 -0400

    More attempts on Herne's wild hunt

--- src/main/java/net/bewitchmentplus/mixin/HerneMultiSummon.java
+++ src/main/java/net/bewitchmentplus/mixin/HerneMultiSummon.java
@@ -1,4 +1,23 @@
 package net.bewitchmentplus.mixin;
 
+import moriyashiine.bewitchment.common.entity.living.HerneEntity;
+import moriyashiine.bewitchment.common.registry.BWEntityTypes;
+import net.bewitchmentplus.common.registry.BWPEntityTypes;
+import net.minecraft.entity.EntityType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+import java.util.Random;
+
+@Mixin(HerneEntity.class)
 public class HerneMultiSummon {
+
+	@Inject(method = "getMinionType", at = @At("RETURN"), remap = false)
+	public EntityType<?> getMinionType() {
+		Random random = new Random();
+		if (random.nextBoolean()) {
+			return BWEntityTypes.WEREWOLF;
+		} else return BWPEntityTypes.DRUDEN;
+	}
 }
