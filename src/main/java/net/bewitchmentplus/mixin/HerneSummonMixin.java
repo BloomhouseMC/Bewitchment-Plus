44d582544299b9769d4790b1772e6546e5ad7503
Sunconure11 <eyespot7b@aol.com>
2021-04-08 05:40:58 -0400

    More druden stuff

--- src/main/java/net/bewitchmentplus/mixin/HerneSummonMixin.java
+++ src/main/java/net/bewitchmentplus/mixin/HerneSummonMixin.java
@@ -1,17 +1,33 @@
 package net.bewitchmentplus.mixin;
 
+import com.google.common.collect.Maps;
 import moriyashiine.bewitchment.common.entity.living.HerneEntity;
+import moriyashiine.bewitchment.common.registry.BWEntityTypes;
 import net.bewitchmentplus.common.registry.BWPEntityTypes;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 
+import java.util.Set;
+
+@SuppressWarnings("ALL")
 @Mixin(HerneEntity.class)
 public class HerneSummonMixin {
 
+	private static final Set<EntityType<net.minecraft.entity.Entity>> SUMMONS;
+
+	static {
+		SUMMONS = (Set<EntityType<net.minecraft.entity.Entity>>) Util.make(Maps.newHashMap(), (hashMap) -> {
+			hashMap.put(BWPEntityTypes.DRUDEN, 1);
+			hashMap.put(BWEntityTypes.WEREWOLF, 2);
+		});
+	}
+
 	@Inject(method = "getMinionType", at = @At("TAIL"), remap = false)
-	public EntityType<?> getMinionType() {
-		return BWPEntityTypes.DRUDEN;
+	private Set<EntityType<Entity>> getMinionType() {
+		return SUMMONS;
 	}
 }
