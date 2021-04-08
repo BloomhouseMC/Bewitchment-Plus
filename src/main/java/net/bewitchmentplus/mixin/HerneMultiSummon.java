fb6e0bf47dd0287b0c5f582c18bd23039275137f
Sunconure11 <eyespot7b@aol.com>
2021-04-08 12:30:20 -0400

    Will be busy

--- src/main/java/net/bewitchmentplus/mixin/HerneMultiSummon.java
+++ src/main/java/net/bewitchmentplus/mixin/HerneMultiSummon.java
@@ -4,20 +4,30 @@ import moriyashiine.bewitchment.common.entity.living.HerneEntity;
 import moriyashiine.bewitchment.common.registry.BWEntityTypes;
 import net.bewitchmentplus.common.registry.BWPEntityTypes;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
 
 @Mixin(HerneEntity.class)
-public class HerneMultiSummon {
+public class HerneMultiSummon extends HerneEntity {
 
-	@Inject(method = "getMinionType", at = @At("RETURN"), remap = false)
-	public EntityType<?> getMinionType() {
+	public HerneMultiSummon(EntityType<? extends HostileEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Inject(method = "getMinionType", at = @At("HEAD"), remap = false, cancellable = true)
+	public EntityType<?> getMinionType(CallbackInfoReturnable<Boolean> callbackInfo) {
 		Random random = new Random();
+		callbackInfo.setReturnValue(true);
 		if (random.nextBoolean()) {
 			return BWEntityTypes.WEREWOLF;
-		} else return BWPEntityTypes.DRUDEN;
+		}
+		callbackInfo.setReturnValue(true);
+		return BWPEntityTypes.DRUDEN;
 	}
 }
