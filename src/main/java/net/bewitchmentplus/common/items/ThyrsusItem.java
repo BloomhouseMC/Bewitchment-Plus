726fc0d51100a84776dfbd5db4b0995d22f11a23
Sunconure11 <eyespot7b@aol.com>
2021-04-10 08:25:43 -0400

    Create ThyrsusItem.java

---
+++ src/main/java/net/bewitchmentplus/common/items/ThyrsusItem.java
@@ -0,0 +1,10 @@
+package net.bewitchmentplus.common.items;
+
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class ThyrsusItem extends SwordItem {
+	public ThyrsusItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+		super(toolMaterial, attackDamage, attackSpeed, settings);
+	}
+}
