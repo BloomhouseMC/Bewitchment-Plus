574ed802fdcb85b44b980aa2b68c58ab07b8c349
Sunconure11 <eyespot7b@aol.com>
2021-04-10 08:30:07 -0400

    Update ThyrsusItem.java

--- src/main/java/net/bewitchmentplus/common/items/ThyrsusItem.java
+++ src/main/java/net/bewitchmentplus/common/items/ThyrsusItem.java
@@ -4,6 +4,7 @@ import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 
 public class ThyrsusItem extends SwordItem {
+
 	public ThyrsusItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
 		super(toolMaterial, attackDamage, attackSpeed, settings);
 	}
