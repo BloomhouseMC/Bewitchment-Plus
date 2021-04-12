eb2a1979bcc1c2b9d08ad5a4f591b9526cc7bba4
Sunconure11 <eyespot7b@aol.com>
2021-04-12 15:23:37 -0400

    Update DrudenEntityRenderer.java

--- src/main/java/net/bewitchmentplus/client/renderer/entity/living/DrudenEntityRenderer.java
+++ src/main/java/net/bewitchmentplus/client/renderer/entity/living/DrudenEntityRenderer.java
@@ -6,18 +6,36 @@ import net.bewitchmentplus.client.model.entity.living.DrudenEntityModel;
 import net.bewitchmentplus.common.entity.living.DrudenEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class DrudenEntityRenderer extends MobEntityRenderer<DrudenEntity, DrudenEntityModel<DrudenEntity>> {
 	private static Identifier[] TEXTURES;
+	private static Identifier[] TEXTURE;
 
 	public DrudenEntityRenderer(EntityRenderDispatcher entityRenderDispatcher) {
 		super(entityRenderDispatcher, new DrudenEntityModel<>(), 0.5f);
 		addFeature(new HeldItemFeatureRenderer<>(this));
+		addFeature(new FeatureRenderer<DrudenEntity, DrudenEntityModel<DrudenEntity>>(this) {
+			@Override
+			public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, DrudenEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+				if (TEXTURE == null) {
+					TEXTURE = new Identifier[1];
+					TEXTURE[0] = new Identifier(BewitchmentPlus.MODID, "textures/entity/living/druden/druden_eyes.png");
+				}
+				VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(TEXTURE[0]));
+				getContextModel().render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, 1, 1, 1, 1);
+			}
+		});
 	}
 
 	@Override
