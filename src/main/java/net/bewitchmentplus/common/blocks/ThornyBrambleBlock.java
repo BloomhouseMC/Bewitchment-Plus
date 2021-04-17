cbe5dbf1aa014a33b0cb7f86a437ab41393ce008
Sunconure11 <eyespot7b@aol.com>
2021-04-16 23:50:28 -0400

    Create ThornyBrambleBlock.java

---
+++ src/main/java/net/bewitchmentplus/common/blocks/ThornyBrambleBlock.java
@@ -0,0 +1,31 @@
+package net.bewitchmentplus.common.blocks;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SugarCaneBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+
+public class ThornyBrambleBlock extends SugarCaneBlock {
+	public ThornyBrambleBlock(Settings settings) {
+		super(settings);
+	}
+
+	@Override
+	public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+		BlockState downState = world.getBlockState(pos.down());
+		return downState.isOf(Blocks.GRASS_BLOCK) || downState.isOf(Blocks.DIRT) || downState.isOf(Blocks.COARSE_DIRT) || downState.isOf(Blocks.PODZOL) || downState.isOf(Blocks.SAND) || downState.isOf(Blocks.RED_SAND);
+	}
+
+	@Override
+	public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+		if (!world.isClient && entity instanceof LivingEntity) {
+			LivingEntity livingEntity = (LivingEntity) entity;
+			entity.damage(DamageSource.CACTUS, 2);
+		}
+	}
+}
