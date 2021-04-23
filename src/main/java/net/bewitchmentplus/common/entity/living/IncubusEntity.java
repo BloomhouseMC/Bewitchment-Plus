d3c05afa2642dcacd052b1af75c79fccaee38151
Sunconure11 <eyespot7b@aol.com>
2021-04-23 18:27:07 -0400

    Update IncubusEntity.java

--- src/main/java/net/bewitchmentplus/common/entity/living/IncubusEntity.java
+++ src/main/java/net/bewitchmentplus/common/entity/living/IncubusEntity.java
@@ -1,6 +1,7 @@
 package net.bewitchmentplus.common.entity.living;
 
 import moriyashiine.bewitchment.api.BewitchmentAPI;
+import moriyashiine.bewitchment.common.entity.living.util.BWHostileEntity;
 import net.minecraft.entity.EntityGroup;
 
 public class IncubusEntity extends BWHostileEntity {
@@ -11,5 +12,10 @@ public class IncubusEntity extends BWHostileEntity {
 
 	protected boolean hasShiny() {
 		return true;
+	}
+
+	@Override
+	public int getVariants() {
+		return 0;
 	}
 }
