3df9e4e3f86e8c3e7fcac1ad7bc9ac478a842e3c
Sunconure11 <eyespot7b@aol.com>
2021-04-26 05:49:56 -0400

    Notes for @DevArcana

--- src/main/java/net/bewitchmentplus/common/entity/living/CambionEntity.java
+++ src/main/java/net/bewitchmentplus/common/entity/living/CambionEntity.java
@@ -77,9 +77,9 @@ public class CambionEntity extends BWHostileEntity {
 		if (barterTimer == 0) {
 			if (itemStack.getItem() == BWPTags.CAMBION_TRADE) {
 				player.playSound(SoundEvents.ITEM_ARMOR_EQUIP_GOLD, SoundCategory.NEUTRAL, 1, 1);
-				ItemStack itemStack2 = ItemUsage.method_30012(itemStack, player, getBarteredItem());
+				ItemStack itemStack2 = ItemUsage.method_30012(itemStack, player, getBarteredItem()); //Fixme: Pick up on a resource table, and give a player a random item, or a random amount of one specific item at a time.
 				player.setStackInHand(hand, itemStack2);
-				barterTimer = 24000;
+				barterTimer = 24000; //Timer exists to avoid cheese
 				return ActionResult.success(this.world.isClient);
 			} else {
 				return super.interactMob(player, hand);
