f60f02998c70ddd08caffaf5fdb5c15ef35b6978
Sunconure11 <eyespot7b@aol.com>
2021-05-11 18:14:24 -0400

    The only thing Logic had ATM.

--- src/main/java/net/bewitchmentplus/api/BWPAPI.java
+++ src/main/java/net/bewitchmentplus/api/BWPAPI.java
@@ -1,4 +1,13 @@
 package net.bewitchmentplus.api;
 
 public class BWPAPI {
+
+		@SuppressWarnings("InstantiationOfUtilityClass")
+		public static final EntityGroup ANGEL = new EntityGroup();
+		@SuppressWarnings("InstantiationOfUtilityClass")
+		public static final EntityGroup FAE = new EntityGroup();
+		@SuppressWarnings("InstantiationOfUtilityClass")
+		public static final EntityGroup SPIRIT = new EntityGroup();
+		@SuppressWarnings("InstantiationOfUtilityClass")
+		public static final EntityGroup ENIGMATIC = new EntityGroup();
 }
