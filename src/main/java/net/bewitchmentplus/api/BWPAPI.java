210f240f3df1ec39de9c7c40c93af6e6e2f5a898
Sunconure11 <eyespot7b@aol.com>
2021-05-16 09:43:41 -0400

    Update BWPAPI.java

--- src/main/java/net/bewitchmentplus/api/BWPAPI.java
+++ src/main/java/net/bewitchmentplus/api/BWPAPI.java
@@ -1,13 +1,18 @@
 package net.bewitchmentplus.api;
 
+import net.minecraft.entity.EntityGroup;
+
 public class BWPAPI {
 
 		@SuppressWarnings("InstantiationOfUtilityClass")
 		public static final EntityGroup ANGEL = new EntityGroup();
+
 		@SuppressWarnings("InstantiationOfUtilityClass")
 		public static final EntityGroup FAE = new EntityGroup();
+
 		@SuppressWarnings("InstantiationOfUtilityClass")
 		public static final EntityGroup SPIRIT = new EntityGroup();
+
 		@SuppressWarnings("InstantiationOfUtilityClass")
 		public static final EntityGroup ENIGMATIC = new EntityGroup();
 }
