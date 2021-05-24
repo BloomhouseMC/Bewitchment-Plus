139f231a30d2eb0f50a97be1e5bc958c941d190d
Sunconure11 <eyespot7b@aol.com>
2021-05-23 20:56:41 -0400

    Cambion armor now renders. Now to get it to fit.

--- src/main/java/net/bewitchmentplus/client/renderer/entity/living/CambionEntityRenderer.java
+++ src/main/java/net/bewitchmentplus/client/renderer/entity/living/CambionEntityRenderer.java
@@ -8,7 +8,9 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
 import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
@@ -18,7 +20,7 @@ public class CambionEntityRenderer extends MobEntityRenderer<CambionEntity, Camb
 	public CambionEntityRenderer(EntityRenderDispatcher entityRenderDispatcher) {
 		super(entityRenderDispatcher, new CambionEntityModel<>(), 0.5f);
 		addFeature(new HeldItemFeatureRenderer<>(this));
-		//addFeature(new ArmorFeatureRenderer<>(this, this.leggingsModel, this.bodyModel));
+		this.addFeature(new ArmorFeatureRenderer<>(this, new BipedEntityModel<>(0.5F), new BipedEntityModel<>(1F)));
 	}
 
 	@Override
