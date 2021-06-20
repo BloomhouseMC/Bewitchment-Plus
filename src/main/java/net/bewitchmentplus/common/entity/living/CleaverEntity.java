8be6fe72eb00fa9d1a79a3e443a58001fa284ffc
Sunconure11 <eyespot7b@aol.com>
2021-06-20 10:59:44 -0400

    Start on cleavers + start on item models

--- src/main/java/net/bewitchmentplus/common/entity/living/CleaverEntity.java
+++ src/main/java/net/bewitchmentplus/common/entity/living/CleaverEntity.java
@@ -1,4 +1,38 @@
 package net.bewitchmentplus.common.entity.living;
 
-public class CleaverEntity {
+import moriyashiine.bewitchment.api.BewitchmentAPI;
+import moriyashiine.bewitchment.common.entity.living.util.BWHostileEntity;
+import net.minecraft.entity.EntityGroup;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.world.World;
+
+public class CleaverEntity extends BWHostileEntity {
+	protected CleaverEntity(EntityType<? extends HostileEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	public EntityGroup getGroup() {
+		return BewitchmentAPI.DEMON;
+	}
+
+	@Override
+	public boolean isFireImmune() {
+		return true;
+	}
+
+	@Override
+	public boolean canUsePortals() {
+		return true;
+	}
+
+	@Override
+	protected boolean hasShiny() {
+		return true;
+	}
+
+	@Override
+	public int getVariants() {
+		return 0;
+	}
 }
