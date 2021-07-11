b83438a2156ab776c845fdb8473f08bccc80357f
Sunconure11 <eyespot7b@aol.com>
2021-07-11 14:35:42 -0400

    Update BewitchmentPlusClient.java

--- src/main/java/net/bewitchmentplus/client/BewitchmentPlusClient.java
+++ src/main/java/net/bewitchmentplus/client/BewitchmentPlusClient.java
@@ -2,6 +2,7 @@ package net.bewitchmentplus.client;
 
 import net.bewitchmentplus.client.renderer.entity.living.BlackDogEntityRenderer;
 import net.bewitchmentplus.client.renderer.entity.living.CambionEntityRenderer;
+import net.bewitchmentplus.client.renderer.entity.living.CleaverEntityRenderer;
 import net.bewitchmentplus.client.renderer.entity.living.DrudenEntityRenderer;
 import net.bewitchmentplus.common.registry.BWPEntityTypes;
 import net.fabricmc.api.ClientModInitializer;
@@ -13,5 +14,6 @@ public class BewitchmentPlusClient implements ClientModInitializer {
 		EntityRendererRegistry.INSTANCE.register(BWPEntityTypes.BLACK_DOG, (entityRenderDispatcher, context) -> new BlackDogEntityRenderer(entityRenderDispatcher));
 		EntityRendererRegistry.INSTANCE.register(BWPEntityTypes.DRUDEN, (entityRenderDispatcher, context) -> new DrudenEntityRenderer(entityRenderDispatcher));
 		EntityRendererRegistry.INSTANCE.register(BWPEntityTypes.CAMBION, (entityRenderDispatcher, context) -> new CambionEntityRenderer(entityRenderDispatcher));
+		EntityRendererRegistry.INSTANCE.register(BWPEntityTypes.CLEAVER, (entityRenderDispatcher, context) -> new CleaverEntityRenderer(entityRenderDispatcher));
 	}
 }
