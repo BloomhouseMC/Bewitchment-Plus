415be38efe78e4fcffdca05d4ad88e41df8b12d6
mrsterner <simon@sterner.dev>
2022-02-27 02:05:35 +0100

    Niffler, Advanced AI behavior part 1

--- src/main/java/dev/mrsterner/bewitchmentplus/common/entity/ai/NifflerSeekGoal.java
+++ src/main/java/dev/mrsterner/bewitchmentplus/common/entity/ai/NifflerSeekGoal.java
@@ -1,45 +1,126 @@
 package dev.mrsterner.bewitchmentplus.common.entity.ai;
 
 import dev.mrsterner.bewitchmentplus.common.entity.NifflerEntity;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.SugarCaneBlock;
+import dev.mrsterner.bewitchmentplus.common.registry.BWPTags;
+import dev.mrsterner.bewitchmentplus.common.utils.RandomPermuteIterator;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.entity.ai.goal.Goal;
-import net.minecraft.state.property.Properties;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Pair;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.LightType;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class NifflerSeekGoal extends Goal {
     public final NifflerEntity niffler;
+    List<BlockPos> blockList = new ArrayList<>();
+    boolean shouldContinue = true;
+    int niffleCooldown = -200;
 
     public NifflerSeekGoal(NifflerEntity niffler) {
         this.niffler = niffler;
     }
 
     @Override
+    public void tick() {
+        if(niffleCooldown < 0){
+            niffleCooldown++;
+        }
+        super.tick();
+    }
+
+
+    @Override
+    public boolean shouldContinue() {
+        return super.shouldContinue() && niffleCooldown == 0;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        try {
+            RandomPermuteIterator randomPermuteIterator = new RandomPermuteIterator(blockList.size());
+            while (randomPermuteIterator.hasMoreElements()){
+                BlockPos chestPos = blockList.get(randomPermuteIterator.nextElement());
+                Inventory inventory = getInventoryAt(niffler.world, chestPos.getX(), chestPos.getY(), chestPos.getZ());
+                List<Pair<ItemStack, Integer>> itemStacks = new ArrayList<>();
+                if(niffler.world.getBlockEntity(chestPos) instanceof ChestBlockEntity){
+                    niffler.world.addSyncedBlockEvent(chestPos, niffler.world.getBlockState(chestPos).getBlock(), 1, 1);
+                }
+                for(int i = 0; i < inventory.size(); i++){
+                    if(BWPTags.NIFFLER.contains(inventory.getStack(i).getItem())){
+                        itemStacks.add(new Pair<>(inventory.getStack(i), i));
+                    }
+                }
+                if(itemStacks.size()>0){
+                    try{
+                        RandomPermuteIterator pickItemAtRandom = new RandomPermuteIterator(itemStacks.size());
+
+                        int k = pickItemAtRandom.nextElement();
+                        System.out.println("RandomSelect: "+k);
+                        ItemStack itemStack = itemStacks.get(k).getLeft();
+                        if(niffler.nifflerInventory.canInsert(itemStack)){
+                            for(int i = 0; i < niffler.nifflerInventory.size(); i++){
+                                if(niffler.nifflerInventory.getStack(i).getItem().equals(itemStack.getItem())){
+                                    niffler.nifflerInventory.getStack(i).increment(1);
+                                    inventory.getStack(itemStacks.get(pickItemAtRandom.nextElement()).getRight()).decrement(1);
+                                    this.niffleCooldown = -200;
+                                    break;
+                                }else if(niffler.nifflerInventory.getStack(i).getItem().equals(Items.AIR)){
+                                    niffler.nifflerInventory.setStack(i, itemStack.split(1));
+                                    this.niffleCooldown = -200;
+                                    break;
+                                }
+                            }
+                        }
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
     public boolean canStart() {
         int rangeCheck = 8;
+        int yRangeCheck = 4;
         BlockPos blockPos = niffler.getBlockPos();
-        List<Block> blockList = new ArrayList<>();
         for(double x = -rangeCheck; x <= rangeCheck; ++x) {
-            for (double y = -rangeCheck; y <= rangeCheck; ++y) {
+            for (double y = -yRangeCheck; y <= yRangeCheck; ++y) {
                 for (double z = -rangeCheck; z <= rangeCheck; ++z) {
                     BlockPos lootPos = new BlockPos(blockPos.getX() + x, blockPos.getY() + y, blockPos.getZ() + z);
-                    blockList.add(niffler.world.getBlockState(lootPos).getBlock());
-
+                    if(niffler.world.getBlockEntity(lootPos) instanceof ChestBlockEntity chestBlockEntity){
+                        blockList.add(chestBlockEntity.getPos());
+                    }
                 }
             }
         }
-        while (blockList.iterator().hasNext()){
+        return !blockList.isEmpty();
+    }
 
+    @Nullable
+    private static Inventory getInventoryAt(World world, double x, double y, double z) {
+        Object blockEntity;
+        Inventory inventory = null;
+        BlockPos blockPos = new BlockPos(x, y, z);
+        BlockState blockState = world.getBlockState(blockPos);
+        Block block = blockState.getBlock();
+        if (block instanceof InventoryProvider) {
+            inventory = ((InventoryProvider)(block)).getInventory(blockState, world, blockPos);
+        } else if (blockState.hasBlockEntity() && (blockEntity = world.getBlockEntity(blockPos)) instanceof Inventory && (inventory = (Inventory)blockEntity) instanceof ChestBlockEntity && block instanceof ChestBlock) {
+            inventory = ChestBlock.getInventory((ChestBlock)block, blockState, world, blockPos, true);
         }
-
-        //List<ChestBlockEntity> list = this.niffler.world.getBlo(ChestBlockEntity.class, this.niffler.getBoundingBox().expand(10,3,10));
-        return false;
+        return inventory;
     }
 }
---
+++ src/main/java/dev/mrsterner/bewitchmentplus/common/utils/RandomPermuteIterator.java
@@ -0,0 +1,32 @@
+package dev.mrsterner.bewitchmentplus.common.utils;
+
+import java.util.Enumeration;
+import java.util.Random;
+
+public class RandomPermuteIterator implements Enumeration<Integer> {
+    int c = 1013904223, a = 1664525;
+    int seed, N, m, next;
+    boolean hasNext = true;
+
+    public RandomPermuteIterator(int N) throws Exception {
+        if (N <= 0 || N > Math.pow(2, 62)) throw new Exception("Unsupported size: " + N);
+        this.N = N;
+        m = (int) Math.pow(2, Math.ceil(Math.log(N) / Math.log(2)));
+        next = seed = new Random().nextInt(N);
+    }
+
+
+
+    @Override
+    public boolean hasMoreElements() {
+        return hasNext;
+    }
+
+    @Override
+    public Integer nextElement() {
+        next = (a * next + c) % m;
+        while (next >= N) next = (a * next + c) % m;
+        if (next == seed) hasNext = false;
+        return  next;
+    }
+}
