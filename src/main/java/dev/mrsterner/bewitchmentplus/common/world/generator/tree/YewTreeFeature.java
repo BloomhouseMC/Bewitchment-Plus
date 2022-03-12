62c8e5f8ec4bf881cbfbf0d1b28a87c5638344f0
mrsterner <simon@sterner.dev>
2022-03-12 14:19:53 +0100

    Some comments on how the gen works

--- src/main/java/dev/mrsterner/bewitchmentplus/common/world/generator/tree/YewTreeFeature.java
+++ src/main/java/dev/mrsterner/bewitchmentplus/common/world/generator/tree/YewTreeFeature.java
@@ -12,10 +12,8 @@ import net.minecraft.structure.StructurePlacementData;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
-import net.minecraft.world.Heightmap;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.util.FeatureContext;
@@ -27,18 +25,24 @@ public class YewTreeFeature extends Feature<DefaultFeatureConfig> {
 
     @Override
     public boolean generate(FeatureContext<DefaultFeatureConfig> context) {
+        //Get structure manager from World, the world we want to place the structure in
         StructureManager structureManager = context.getWorld().toServerWorld().getStructureManager();
+        //Try fetch the nbt with the structure manager
         Optional<Structure> structureOptional = structureManager.getStructure(new Identifier(BewitchmentPlus.MODID, "features/trees/yew_tree1"));
         if (structureOptional.isEmpty()) {
             System.out.println("NBT does not exist!");
             return false;
         }
+        //Unless structureOptional.isEmpty() not catches, get the structure from the optional
         Structure structure = structureOptional.get();
-        BlockPos newOrigin = context.getOrigin().subtract(new Vec3i(Math.floor((double) structure.getSize().getX() / 2),0,Math.floor((double) structure.getSize().getX() / 2)));
-        BlockPos top = context.getWorld().getTopPosition(Heightmap.Type.WORLD_SURFACE, newOrigin);
-        if (context.getWorld().getBlockState(newOrigin.down()).getBlock() == Blocks.GRASS_BLOCK) {
-            var placementsettings = (new StructurePlacementData()).setMirror(BlockMirror.NONE).setRotation(BlockRotation.NONE).setIgnoreEntities(false);
-            structure.place(context.getWorld(), newOrigin, context.getOrigin(), placementsettings, context.getRandom(), 2);
+        //Change the origin from the corner of the structure to the middle of the structure
+        BlockPos normalizeOrigin = context.getOrigin().subtract(new Vec3i(Math.floor((double) structure.getSize().getX() / 2),0,Math.floor((double) structure.getSize().getX() / 2)));
+        //Check if the normalizedOrigin is in fact on top of a grass block
+        if (context.getWorld().getBlockState(normalizeOrigin.down()).getBlock() == Blocks.GRASS_BLOCK) {
+            //Get basic placementData
+            var placementData = (new StructurePlacementData()).setMirror(BlockMirror.NONE).setRotation(BlockRotation.NONE).setIgnoreEntities(false);
+            //Place the structure at the normalized origin
+            structure.place(context.getWorld(), normalizeOrigin, context.getOrigin(), placementData, context.getRandom(), 2);
             return true;
         }else{
             return false;
