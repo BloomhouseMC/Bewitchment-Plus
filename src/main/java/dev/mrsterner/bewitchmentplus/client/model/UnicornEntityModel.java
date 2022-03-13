3cb9e8f20ccb5481ddf17933b4ec95df37c02685
mrsterner <simon@sterner.dev>
2022-03-13 20:35:48 +0100

    clean up

--- src/main/java/dev/mrsterner/bewitchmentplus/client/model/UnicornEntityModel.java
+++ src/main/java/dev/mrsterner/bewitchmentplus/client/model/UnicornEntityModel.java
@@ -15,14 +15,12 @@ public class UnicornEntityModel<T extends UnicornEntity> extends HorseEntityMode
 
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = HorseEntityModel.getModelData(Dilation.NONE);
-		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData modelPartData3 = modelPartData.getChild("head_parts").getChild("head");
-		modelPartData3.addChild("horn", ModelPartBuilder.create().uv(24, 37).cuboid(-0.5F, -12.0F, -1.5F, 1.0F, 6.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -5.0F, 4.0F));
-
+		ModelPartData head = modelData.getRoot().getChild("head_parts").getChild("head");
+		head.addChild("horn", ModelPartBuilder.create().uv(24, 37).cuboid(-0.5F, -12.0F, -1.5F, 1.0F, 6.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -5.0F, 4.0F));
 		return TexturedModelData.of(modelData, 64, 64);
 	}
 
-	public void setAngles(T abstractDonkeyEntity, float f, float g, float h, float i, float j) {
-		super.setAngles(abstractDonkeyEntity, f, g, h, i, j);
+	public void setAngles(T unicornEntity, float f, float g, float h, float i, float j) {
+		super.setAngles(unicornEntity, f, g, h, i, j);
 	}
 }
