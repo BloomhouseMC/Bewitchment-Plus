8d3d67250e0312cccc9474ee9790c360a74eacd0
mrsterner <simon@sterner.dev>
2022-03-14 00:37:19 +0100

    Halflife no milk

---
+++ src/main/java/dev/mrsterner/bewitchmentplus/mixin/common/MilkBucketMixin.java
@@ -0,0 +1,23 @@
+package dev.mrsterner.bewitchmentplus.mixin.common;
+
+import dev.mrsterner.bewitchmentplus.common.registry.BWPCurses;
+import moriyashiine.bewitchment.common.registry.BWComponents;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.MilkBucketItem;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MilkBucketItem.class)
+public class MilkBucketMixin {
+    @Inject(method = "finishUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;clearStatusEffects()Z", shift = At.Shift.BEFORE))
+    private void stopMilkWhenHalfLife(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir){
+        if(BWComponents.CURSES_COMPONENT.get(user).hasCurse(BWPCurses.HALF_LIFE)){
+            cir.setReturnValue(stack.isEmpty() ? new ItemStack(Items.BUCKET) : stack);
+        }
+    }
+}
