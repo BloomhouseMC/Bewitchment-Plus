5c33e832f4e1096f6edc1350bb9237c5087fb407
mrsterner <simon@sterner.dev>
2022-03-22 13:54:56 +0100

    Phoenix AI

--- src/main/java/dev/mrsterner/bewitchmentplus/common/entity/PhoenixEntity.java
+++ src/main/java/dev/mrsterner/bewitchmentplus/common/entity/PhoenixEntity.java
@@ -1,20 +1,30 @@
 package dev.mrsterner.bewitchmentplus.common.entity;
 
+import moriyashiine.bewitchment.common.entity.living.OwlEntity;
 import moriyashiine.bewitchment.common.entity.living.util.BWTameableEntity;
+import moriyashiine.bewitchment.common.registry.BWSoundEvents;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.control.FlightMoveControl;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.BirdNavigation;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.PassiveEntity;
-import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.passive.*;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class PhoenixEntity extends BWTameableEntity {
     public PhoenixEntity(EntityType<? extends TameableEntity> type, World world) {
         super(type, world);
+        this.moveControl = new FlightMoveControl(this, 180, false);
     }
 
     public static DefaultAttributeContainer.Builder createAttributes() {
@@ -25,9 +35,49 @@ public class PhoenixEntity extends BWTameableEntity {
         .add(EntityAttributes.GENERIC_FLYING_SPEED, 0.6D);
     }
 
+    protected void initGoals() {
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1, new SitGoal(this));
+        this.goalSelector.add(2, new MeleeAttackGoal(this, 1.0D, true));
+        this.goalSelector.add(3, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
+        this.goalSelector.add(4, new AnimalMateGoal(this, 1.0D));
+        this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.0D));
+        this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.add(6, new LookAroundGoal(this));
+        this.targetSelector.add(0, new TrackOwnerAttackerGoal(this));
+        this.targetSelector.add(1, new AttackWithOwnerGoal(this));
+        this.targetSelector.add(2, new RevengeGoal(this, new Class[0]));
+        this.targetSelector.add(3, new UntamedActiveTargetGoal(this, AnimalEntity.class, false, (entity) -> {
+            return entity instanceof ChickenEntity || entity instanceof RabbitEntity;
+        }));
+    }
+
     @Override
     protected boolean hasShiny() {
         return false;
+    }
+
+    protected void addFlapEffects() {
+        this.playSound(BWSoundEvents.ENTITY_OWL_FLY, 0.15F, 1.0F);
+    }
+
+    protected boolean hasWings() {
+        return true;
+    }
+
+    public boolean handleFallDamage(float fallDistance, float damageMultiplier, DamageSource damageSource) {
+        return false;
+    }
+
+    protected void fall(double heightDifference, boolean onGround, BlockState landedState, BlockPos landedPosition) {
+    }
+
+    protected EntityNavigation createNavigation(World world) {
+        BirdNavigation birdNavigation = new BirdNavigation(this, world);
+        birdNavigation.setCanPathThroughDoors(false);
+        birdNavigation.setCanSwim(true);
+        birdNavigation.setCanEnterOpenDoors(true);
+        return birdNavigation;
     }
 
     @Override
