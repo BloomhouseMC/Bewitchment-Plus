f71d083906fcfe7e63d0eba537e9a635770e8337
mrsterner <simon@sterner.dev>
2022-05-05 16:21:38 +0200

    Looking at death for too long will inflict slowness

--- src/main/java/dev/mrsterner/bewitchmentplus/common/entity/DeathEntity.java
+++ src/main/java/dev/mrsterner/bewitchmentplus/common/entity/DeathEntity.java
@@ -1,5 +1,6 @@
 package dev.mrsterner.bewitchmentplus.common.entity;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
@@ -8,13 +9,18 @@ import net.minecraft.entity.ai.control.FlightMoveControl;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.mob.EndermanEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -38,19 +44,31 @@ public class DeathEntity extends HostileEntity implements IAnimatable {
     }
 
 
-
+    boolean isPlayerStaring(PlayerEntity player) {
+        ItemStack itemStack = player.getInventory().armor.get(3);
+        if (itemStack.isOf(Blocks.CARVED_PUMPKIN.asItem())) {
+            return false;
+        } else {
+            Vec3d vec3d = player.getRotationVec(1.0F).normalize();
+            Vec3d vec3d2 = new Vec3d(this.getX() - player.getX(), this.getEyeY() - player.getEyeY(), this.getZ() - player.getZ());
+            double d = vec3d2.length();
+            vec3d2 = vec3d2.normalize();
+            double e = vec3d.dotProduct(vec3d2);
+            return e > 1.0 - 0.025 / d && player.canSee(this);
+        }
+    }
 
     @Override
     protected void initGoals() {
         super.initGoals();
-
         //this.goalSelector.add(1, new DeathHoverGoal(this, 1));
         this.goalSelector.add(0, new SwimGoal(this));
         this.goalSelector.add(1, new MeleeAttackGoal(this, 1, true));//TODO make death sickle attack goal instead
         this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.add(2, new SlownessLookGoal(this));
         this.goalSelector.add(2, new LookAroundGoal(this));
         this.targetSelector.add(1, new RevengeGoal(this));
-        this.targetSelector.add(2, new ActiveTargetGoal(this, LivingEntity.class, 0, false, false, CAN_ATTACK_PREDICATE));
+        this.targetSelector.add(2, new ActiveTargetGoal<>(this, LivingEntity.class, 0, false, false, CAN_ATTACK_PREDICATE));
     }
 
     @Override
@@ -109,11 +127,42 @@ public class DeathEntity extends HostileEntity implements IAnimatable {
         return factory;
     }
 
-    static class DeathHoverGoal extends FlyGoal {
+    private static class DeathHoverGoal extends FlyGoal {
         public DeathHoverGoal(PathAwareEntity pathAwareEntity, double d) {
             super(pathAwareEntity, d);
             this.setControls(EnumSet.of(Goal.Control.MOVE, Goal.Control.JUMP, Goal.Control.LOOK));
         }
 
+    }
+
+    private static class SlownessLookGoal extends Goal {
+        private final DeathEntity deathEntity;
+        @Nullable private LivingEntity target;
+        public SlownessLookGoal(DeathEntity deathEntity) {
+            this.deathEntity = deathEntity;
+        }
+
+        @Override
+        public void start() {
+            if(this.target instanceof PlayerEntity player){
+                player.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 20 * 3));
+            }
+            super.start();
+        }
+
+        @Override
+        public boolean canStart() {
+            target = this.deathEntity.getTarget();
+            if (target instanceof PlayerEntity player) {
+                double d = target.squaredDistanceTo(this.deathEntity);
+                return !(d > 256.0) && this.deathEntity.isPlayerStaring(player) && !player.hasStatusEffect(StatusEffects.SLOWNESS);
+            } else {
+                return false;
+            }
+        }
+        @Override
+        public void tick() {
+            this.deathEntity.getLookControl().lookAt(this.target.getX(), this.target.getEyeY(), this.target.getZ());
+        }
     }
 }
